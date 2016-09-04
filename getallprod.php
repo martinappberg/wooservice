56da46dd538ec47a236ae410877b2e515a4ad692
martinappberg <mkjberg@gmail.com>
2016-09-04 16:02:48 +0200

    Update getallprod.php

--- getallprod.php
+++ getallprod.php
@@ -20,6 +20,7 @@ try {
 	$client = new WC_API_Client( $store_url, $consumer_key, $consumer_secret, $options );
 	$args = array(
         //'filter[limit]' => 6,
+        'per_page' => 50,
         'filter[orderby]' => 'meta_value_num',
         'filter[order]' => 'DESC',
         'filter[orderby_meta_key]' => 'total_sales',
